804072df669b384d61fe00ddeae2ef30d9508725
Jonathan Roth <JonathanRoth@protonmail.com>
2022-05-09 10:22:32 -0400

    added description to setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,15 @@
 
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+
+
 setup(
     name="bumbling",
-    version="1.0",
+    version="1.1",
     license="MIT",
     author="Jonathan Roth",
     author_email="JonathanRoth@protonmail.com",
@@ -15,4 +21,6 @@ setup(
     url="https://github.com/JonathanRoth13/bumbling",
     install_requires=["PyExifTool"],
     entry_points={"console_scripts": ["bumbling=src.bumbling:main"]},
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
 )
